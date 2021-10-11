a60720b5714857bdcd0808ff101dd2e1ff49ee2f
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-10-10 23:01:49 -0400

    Refactored JS file / Passing all test

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 
-let form = document.querySelector('form')
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const location = event.target.search.value
-    //uppercase first letter of input
+    const form = document.querySelector("form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const location = event.target.search.value
+        const city = location[0].toUpperCase() + location.slice(1);
+        
+        fetch(`https://wttr.in/${location}?format=j1`)
+        .then(response => response.json())
+        .then(data => {
 
-    fetch(`https://wttr.in/${location}?format=j1`)
-    .then(response => response.json())
-    .then(data => {
-        //today
+        // Display
+        const area = data.nearest_area[0].areaName[0].value;
+        const region = data.nearest_area[0].region[0].value;
+        const country = data.nearest_area[0].country[0].value;
+        const currentWeather = data.current_condition[0].FeelsLikeF;
+        // Today
         const todayAvgTemp = data.weather[0].avgtempF;
         const todayMaxTemp = data.weather[0].maxtempF;
         const todayMinTemp = data.weather[0].mintempF;
-        //tomorrow
+        // Tomorrow
         const tmrwAvgTemp = data.weather[1].avgtempF;
         const tmrwMaxTemp = data.weather[1].maxtempF;
         const tmrwMinTemp = data.weather[1].mintempF;
-        // day after
-        const AfterAvgTemp = data.weather[2].avgtempF;
-        const AfterMaxTemp = data.weather[2].maxtempF;
-        const AfterMinTemp = data.weather[2].mintempF;
+        // Day after
+        const afterAvgTemp = data.weather[2].avgtempF;
+        const afterMaxTemp = data.weather[2].maxtempF;
+        const afterMinTemp = data.weather[2].mintempF;
         
-        // Selecting display section of grid and inserting data dynamically
+        // applying html elements and inserting data dynamically
         const display = document.querySelector(".display")
-            display.innerHTML =
+        const content = display.innerHTML =
                 
                 `<div class ="main-display-container">
-                <h2>${location}</h2>
-                <div id="display-area"><strong>Area</strong>: ${data.nearest_area[0].areaName[0].value}</div>
+                <h2>${city}</h2>
+                <br>
+                <div id="display-area"><strong>Area</strong>: ${area}</div>
                 <br> 
-                <div id="display-region"><strong>Region</strong>: ${data.nearest_area[0].region[0].value}</div> 
+                <div id="display-region"><strong>Region</strong>: ${region}</div> 
                 <br>
-                <div id="display-country"><strong>Country</strong>: ${data.nearest_area[0].country[0].value}</div> 
+                <div id="display-country"><strong>Country</strong>: ${country}</div> 
                 <br>
-                <div id="display-current"><strong>Currently</strong>: Feels like ${data.current_condition[0].FeelsLikeF}°F</div>             
+                <div id="display-current"><strong>Currently</strong>: Feels like ${currentWeather}°F</div>             
                 </div>
                 <div class="todays-forecast-container">
                 <h3>Today</h3>
@@ -53,63 +60,31 @@ form.addEventListener('submit', (event) => {
                 </div>
                 <div class="day-after-forecast-container">
                 <h3>Day After Tomorrow</h3>
-                <div id="after-avg"><strong>Average Temperature</strong>: ${AfterAvgTemp}°F</div>
+                <div id="after-avg"><strong>Average Temperature</strong>: ${afterAvgTemp}°F</div>
                 <br>
-                <div id="after-max"><strong>Max Temperature</strong>: ${AfterMaxTemp}°F</div>
+                <div id="after-max"><strong>Max Temperature</strong>: ${afterMaxTemp}°F</div>
                 <br>
-                <div id="after-min"><strong>Min Temperature</strong>:${AfterMinTemp}°F</div>
+                <div id="after-min"><strong>Min Temperature</strong>: ${afterMinTemp}°F</div>
                 </div>`;
 
-        //sidebar .history
-        let sidebar = document.querySelector(".history")
-        let li = document.createElement("li")
+        // Adds previous searches to Sidebar
+        const sidebar = document.querySelector(".history")
+        const li = document.createElement("li")
         li.classList.add("prev-search")
+        const ul = document.querySelector(".prev-placeholder")
         li.innerHTML = 
-        `<a href="">${data.nearest_area[0].areaName[0].value}</a><span> - ${data.current_condition[0].FeelsLikeF}°F</span>`;
+        `<a href="">${city}</a><span> - ${data.current_condition[0].FeelsLikeF}°F</span>`;
         sidebar.append(li);
+        ul.innerHTML = "";
 
+        // updates display with city selected from previous search sidebar
         li.addEventListener("click", (event)=>{
             event.preventDefault();
             const display = document.querySelector(".display")
-            display.innerHTML =
-                
-                `<div class ="main-display-container">
-                <h2>${location}</h2>
-                <div id="display-area"><strong>Area</strong>: ${data.nearest_area[0].areaName[0].value}</div>
-                <br> 
-                <div id="display-region"><strong>Region</strong>: ${data.nearest_area[0].region[0].value}</div> 
-                <br>
-                <div id="display-country"><strong>Country</strong>: ${data.nearest_area[0].country[0].value}</div> 
-                <br>
-                <div id="display-current"><strong>Currently</strong>: Feels like ${data.current_condition[0].FeelsLikeF}°F</div>             
-                </div>
-                <div class="todays-forecast-container">
-                <h3>Today</h3>
-                <div id="today-avg"><strong>Average Temperature</strong>: ${todayAvgTemp}°F</div>
-                <br>
-                <div id="today-max"><strong>Max Temperature</strong>: ${todayMaxTemp}°F</div>
-                <br>
-                <div id="today-min"><strong>Min Temperature</strong>: ${todayMinTemp}°F</div>
-                </div>
-                <div class="tomorrows-forecast-container">
-                <h3>Tomorrow</h3>
-                <div id="tomorrow-avg"><strong>Average Temperature</strong>: ${tmrwAvgTemp}°F</div>
-                <br>
-                <div id="tomorrow-max"><strong>Max Temperature</strong>: ${tmrwMaxTemp}°F</div>
-                <br>
-                <div id="tomorrow-min"><strong>Min Temperature</strong>: ${tmrwMinTemp}°F</div>
-                </div>
-                <div class="day-after-forecast-container">
-                <h3>Day After Tomorrow</h3>
-                <div id="after-avg"><strong>Average Temperature</strong>: ${AfterAvgTemp}°F</div>
-                <br>
-                <div id="after-max"><strong>Max Temperature</strong>: ${AfterMaxTemp}°F</div>
-                <br>
-                <div id="after-min"><strong>Min Temperature</strong>:${AfterMinTemp}°F</div>
-                </div>`;
-    })
-    
+            display.innerHTML = content;
+            })
         })
     .catch((err) =>
     console.log(err))
+    event.target.reset()
 });
